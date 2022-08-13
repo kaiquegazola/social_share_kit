015359ee61e2e73fc4cb0d291efadd8fb822a949
Mateus Siqueira <54752439+mateussiqueira@users.noreply.github.com>
2022-08-13 14:48:34 -0300

    add SocialShareAdapter

--- lib/src/domain/infra/instagram_share_adapter.dart
+++
@@ -1,7 +0,0 @@
-import 'package:social_share_kit/src/domain/domain.dart';
-
-class InstragramShareAdapter {
-  const InstragramShareAdapter({required this.socialShare});
-
-  final SocialShare socialShare;
-}
