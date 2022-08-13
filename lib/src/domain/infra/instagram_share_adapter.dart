acd60d61e97f28c52d96cee6ca6e19dcd509a0c4
Mateus Siqueira <54752439+mateussiqueira@users.noreply.github.com>
2022-08-13 14:48:04 -0300

    add SocialShareAdapter

---
+++ lib/src/domain/infra/instagram_share_adapter.dart
@@ -0,0 +1,7 @@
+import 'package:social_share_kit/src/domain/domain.dart';
+
+class InstragramShareAdapter {
+  const InstragramShareAdapter({required this.socialShare});
+
+  final SocialShare socialShare;
+}
---
+++ lib/src/domain/infra/social_share_adapter.dart
@@ -0,0 +1,9 @@
+import 'package:social_share_kit/src/domain/domain.dart';
+
+class SocialShareAdapter implements SocialShare {
+  @override
+  Future<void> share() {
+    // TODO: implement share
+    throw UnimplementedError();
+  }
+}
