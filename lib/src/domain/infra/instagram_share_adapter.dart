fda82de6ad7e7e03eff99750c9b34890a04f1958
Mateus Siqueira <54752439+mateussiqueira@users.noreply.github.com>
2022-08-13 14:51:56 -0300

    add InstragramShareAdapter

---
+++ lib/src/domain/infra/instagram_share_adapter.dart
@@ -0,0 +1,25 @@
+import 'package:social_share_kit/src/domain/domain.dart';
+
+class InstragramShareAdapter {
+  const InstragramShareAdapter({required this.socialShare});
+
+  final SocialShare socialShare;
+
+  Future<void> story() async {
+    try {
+      await socialShare.share();
+    } catch (_) {}
+  }
+
+  Future<void> post() async {
+    try {
+      await socialShare.share();
+    } catch (_) {}
+  }
+
+  Future<void> direct() async {
+    try {
+      await socialShare.share();
+    } catch (_) {}
+  }
+}
